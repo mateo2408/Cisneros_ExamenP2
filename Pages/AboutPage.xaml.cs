0cc9020391d3172841f91507a5bba20681f9982b
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2025-06-02 07:39:09 -0500

    Creacion AboutPage

---
+++ Pages/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cisneros_ExamenP2.Pages;
+
+public partial class AboutPage : ContentPage
+{
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
+}
