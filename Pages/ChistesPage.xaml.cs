e6a765527ff5f316f5f88bbddbb007a3e4e4edee
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2025-06-02 07:38:39 -0500

    Codificacion de ChistesPage

--- Pages/ChistesPage.xaml.cs
+++ Pages/ChistesPage.xaml.cs
@@ -1,15 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cisneros_ExamenP2.Pages;
-
 public partial class ChistesPage : ContentPage
 {
     public ChistesPage()
     {
         InitializeComponent();
+        LoadJoke();
+    }
+
+    private async void LoadJoke()
+    {
+        using var client = new HttpClient();
+        var response = await client.GetStringAsync("https://official-joke-api.appspot.com/random_joke");
+        var joke = System.Text.Json.JsonSerializer.Deserialize<Joke>(response);
+        JokeLabel.Text = $"{joke.setup}\n\n{joke.punchline}";
+    }
+
+    private void OnOtroChisteClicked(object sender, EventArgs e)
+    {
+        LoadJoke();
+    }
+
+    class Joke
+    {
+        public string setup { get; set; }
+        public string punchline { get; set; }
     }
 }
