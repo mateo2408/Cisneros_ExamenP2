50ca763bd22b9ed70d8c8fe694ae21979b63d124
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2025-06-02 07:31:28 -0500

    Creacion de Main Page y agregar imagen a proyecto

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Cisneros_ExamenP2;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Cisneros_ExamenP2;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnChistesClicked(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnAboutClicked(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
