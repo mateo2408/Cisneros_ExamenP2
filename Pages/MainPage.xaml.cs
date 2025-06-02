59d2000982bde4b4aec4bfdac56e1ec77d3a7df3
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2025-06-02 07:37:40 -0500

    Cambio de Nombre de archivos, Organizacion de archivos, creacion de ChistesPage

---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Cisneros_ExamenP2;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnChistesClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ChistesPage());
+    }
+    
+    private async void OnAboutClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AboutPage());
+    }
+}
